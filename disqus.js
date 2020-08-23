83a2fda055a52f6e14ed767b10aeeb1b5756d75d
Thibault Wittemberg <twittemberg@genetec.com>
2020-08-23 17:35:04 -0400

    Publish deploy 2020-08-23 17:35

---
+++ disqus.js
@@ -0,0 +1,5 @@
+(function() {
+    var t = document,
+        e = t.createElement("script");
+    e.src = "https://thibaultwittemberg.disqus.com/embed.js", e.setAttribute("data-timestamp", +new Date), (t.head || t.body).appendChild(e)
+})();
